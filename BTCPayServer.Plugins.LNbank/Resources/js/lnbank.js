5639c436f47e24ecee8f38c0cc75306c73a2cc48
Dennis Reimann <mail@dennisreimann.de>
2023-01-30 15:39:19 +0100

    Reformat assets

--- BTCPayServer.Plugins.LNbank/Resources/js/lnbank.js
+++ BTCPayServer.Plugins.LNbank/Resources/js/lnbank.js
@@ -3,21 +3,21 @@
 const isDev = document.documentElement.hasAttribute("data-devenv");
 
 document.addEventListener("DOMContentLoaded", () => {
-    const {hubs} = window.LNbank
+  const {hubs} = window.LNbank
 
-    // SignalR
-    hubs.forEach(hub => {
-        const connection = new signalR.HubConnectionBuilder()
-            .withUrl(`/plugins/lnbank/hubs/${hub.id}`)
-            .withAutomaticReconnect()
-            .build()
+  // SignalR
+  hubs.forEach(hub => {
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl(`/plugins/lnbank/hubs/${hub.id}`)
+      .withAutomaticReconnect()
+      .build()
 
-        Object.entries(hub.handlers).forEach(([message, handler]) => {
-            if (isDev) connection.on(message, console.debug)
-            connection.on(message, handler)
-        })
-
-        connection.start()
-            .catch(console.error)
+    Object.entries(hub.handlers).forEach(([message, handler]) => {
+      if (isDev) connection.on(message, console.debug)
+      connection.on(message, handler)
     })
+
+    connection.start()
+      .catch(console.error)
+  })
 })
