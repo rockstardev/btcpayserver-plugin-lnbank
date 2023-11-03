c818992d7fbc2a246e1d56f8b1b82fdb3a4c97e7
Dennis Reimann <mail@dennisreimann.de>
2023-11-03 20:19:35 +0100

    Update BTCPay Server version requirement

--- BTCPayServer.Plugins.LNbank/LNbankPlugin.cs
+++ BTCPayServer.Plugins.LNbank/LNbankPlugin.cs
@@ -16,7 +16,7 @@ public class LNbankPlugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     {
-        new () { Identifier = nameof(BTCPayServer), Condition = ">=1.11.6" }
+        new () { Identifier = nameof(BTCPayServer), Condition = ">=1.11.7" }
     };
 
     public override void Execute(IServiceCollection services)
